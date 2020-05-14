57cd77830624ec7eb85a5cdcecc1a36ac760aaaf
LizyPk <31969078+LizyPk@users.noreply.github.com>
2020-05-14 20:17:32 -0300

    add JS script
    Adicionando o arquivo JS e testando suas chamadas

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+/* JS DE TESTE DE ACESSO E CALL (por enquanto)*/
+
+String = nameProject /*  */
+
+function homePage() {
+    window.alert('HOME!')
+}
+
+function projectsPage() {
+    window.alert('PROJECTS!')
+}
+
+function gamesPage() {
+    window.alert('GAMES!')
+}
+
+/* Esta função deve abrir o mesmo template, porém de acordo com o projeto passado. */
+function gameProjectPage(nameProject) {
+    window.alert(`PAGINA DO ${nameProject}!`)
+}
+
+function teamPage() {
+    window.alert('TEAM!')
+}
